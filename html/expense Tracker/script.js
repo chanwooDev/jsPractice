573c403e115baab2a4b0c3c34c99156583c31ee4
knuchanwoo <39613774+knuchanwoo@users.noreply.github.com>
2021-05-27 01:00:47 +0900

    Add files via upload

--- html/expense Tracker/script.js
+++ html/expense Tracker/script.js
@@ -3,27 +3,31 @@ const moneyplus = document.getElementById('moneyplus');
 const moneyminus = document.getElementById('moneyminus');
 const list = document.getElementById('list');
 const form = document.getElementById('form');
-const text = document.getElementById('txet');
-const amount = document.getAnimations('amount');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
 
 
 
 function addTransaction(e){
-  
+
+
   const transaction = {
-    text:text.nodeValue;
-    amount:amount.nodeValue;
-      
-  }
-  var string = text.nodeValue;
-  var 
+    text:text.value,
+    amount:amount.value
+  };
+  setBalance(transaction.amount);
+  //addHistory(transaction);
+
   //History에 추가
-  
-  //amount => moneyplus or moneyminus
+    //amount => moneyplus or moneyminus
   //balance 계산
   //
-
 } 
+function setBalance(money){
+  var curMoney=Number(balance.innerText.replace(/[^0-9.]/g, ""));
+  curMoney += money;
+  balance.innerText = "hello";
+}
 
 
 form.addEventListener('submit', addTransaction);
